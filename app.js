5f188d7fcf33be15f7c0d5f824b8e868dc7c83ea
behzat bilgin erdem <bbilgin.erdem@gmail.com>
2022-10-11 11:51:00 +0300

    folder structure changed

--- app.js
+++ app.js
@@ -2,6 +2,16 @@ const express = require('express')
 
 const app = express()
 
+// Template Engine
+app.set("view engine", "ejs")
+
+// Middlewares
+app.use(express.static("public"))
+
+app.get('/', (req, res) => {
+	res.send('Hello World!')
+})
+
 const port = 3000
 app.listen(port, () => {
 	console.log(`Example app listening at http://localhost:${port}`)
