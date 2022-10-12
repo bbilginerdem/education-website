5c0dd60e61d66ded41daad5522c568d128babf8a
behzat bilgin erdem <bbilgin.erdem@gmail.com>
2022-10-12 07:43:39 +0300

    course route created

--- app.js
+++ app.js
@@ -1,7 +1,13 @@
 const express = require('express')
 const pageRoute = require('./routes/pageRoute')
+const courseRoute = require('./routes/courseRoute')
 
 const app = express()
+
+// Connect to the DB server
+mongoose.connect('mongodb://localhost/smartedu-db').then(() => {
+	console.log('Connected to MongoDB')
+})
 
 // Template Engine
 app.set('view engine', 'ejs')
@@ -11,6 +17,7 @@ app.use(express.static('public'))
 
 // Routes
 app.use('/', pageRoute)
+app.use('/courses', courseRoute)
 
 const port = 3000
 app.listen(port, () => {
